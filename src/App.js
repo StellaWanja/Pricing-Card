417f9e4b8329a7fa420d05402ffadfadd7f6e9e9
StellaWanja <stylades@gmail.com>
2021-05-04 14:51:28 +0300

    first-commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,54 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import {FaCheckCircle} from 'react-icons/fa';
+import data from './data';
 
 function App() {
+
+  const [items, setItems] = useState(data);
+  const [value, setValue] = useState(0);
+
+  const {title, desc, price, info, lists} = items[value];
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+      <section className='container'>
+        <div className='btn-container'>
+          {items.map((item, index) => {
+            return(
+              <button 
+                key={index} 
+                onClick={(() => setValue(index))}
+                className={`item-btn ${index === value && 'active-btn'}`}
+              >
+                {item.title}            
+              </button>
+            )
+          })}
+        </div>
+        <div className='info-container'>
+          <h4>{title}</h4>
+          <div className='title'>
+            <h2>{desc}</h2>
+            <h2>${price}</h2>
+          </div>
+          <p>{info}</p>
+          {lists.map((list, index) => {
+            return(
+              <div key={index} className='list-div'>
+                <p>
+                  <span><FaCheckCircle className='check-mark'/></span>
+                  {list}
+                </p>
+              </div>
+              );
+            })}
+        </div>
+
+        <button className='cart-btn'>Add to cart</button>
+
+      </section>
+    </main>
   );
 }
 
---
+++ src/data.js
@@ -0,0 +1,39 @@
+const data = [
+    {
+        id: 1,
+        title: 'basic',
+        desc: 'icon sets',
+        price: 55,
+        info: 'Up to 5 creative & professional icons + one color versions/themes.',
+        lists: [
+            'Included source files', 
+            'Responsive components'
+        ]
+    },
+    {
+        id: 2,
+        title: 'standard',
+        desc: 'icon sets',
+        price: 120,
+        info: 'Up to 10 creative & professional icons + two color versions/themes.',
+        lists: [
+            'Included source files', 
+            'Responsive components',
+            'Transparency'
+        ]
+    },
+    {
+        id: 3,
+        title: 'premium',
+        desc: 'icon sets',
+        price: 200,
+        info: 'Up to 25 creative & professional icons + three color versions/themes.',
+        lists: [
+            'Included source files', 
+            'Responsive components',
+            'Transparency'
+        ]
+    }
+]
+
+export default data;
